d636e702e57316e650e0fc1e05b2d9923df1e68d
Mavlid <votkny93@gmail.com>
2026-03-09 18:47:28 +0300

    fix error two

--- homework-5.js
+++ homework-5.js
@@ -1,51 +1,50 @@
 // Задание №1: Создал функцию, которая принимает 2 параметра: город и температуру и выводит сообщение в консоль.
 
 function showWeather(city, temperature) {
-console.log(`Сейчас в ${city} температура - ${temperature} градусов по Цельсию`);
+  console.log(`Сейчас в ${city} температура - ${temperature} градусов по Цельсию`);
 }
 showWeather("Москва", 20);
 
 // Задание №2: Создал переменную, которая хранит внутри себя скорость света; Добавил функцию сравнения скорости со скоростью света.
 
-const speedLight = 299792458;
-function checkSpeed(speed) {
-  if (speed > speedLight) {
-    console.log( "Сверхсветовая скорость");
-  }
-  else if (speed < speedLight) {
+const SPEED_LIGHT = 299792458;
+function compareSpeed(speed) {
+  if (speed > SPEED_LIGHT) {
+    console.log("Сверхсветовая скорость");
+  } else if (speed < SPEED_LIGHT) {
     console.log("Субсветовая скорость");
-  }
-  else {
+  } else {
     console.log("Скорость света");
   }
 }
- checkSpeed(1000000000);
- checkSpeed(300000);
- checkSpeed(299792458);
+compareSpeed(1000000000);
+compareSpeed(300000);
+compareSpeed(299792458);
 
- // Задание №3: Создал переменную №1, которая содержит продукт и переменную №2, которая содержит его цену;
+// Задание №3: Создал переменную №1, которая содержит продукт и переменную №2, которая содержит его цену;
 
 const fruit = 'apple';
 const price = 20;
-function calculate(budget) {
+function buyProduct(budget) {
   if (budget >= price) {
-    console.log(`${fruit} приобретен.Спасибо за покупку!`); 
-  } 
+    console.log(`${fruit} приобретен.Спасибо за покупку!`);
+  }
   else {
     console.log(`Вам не хватает ${price - budget}$, пополните баланс`);
   }
 }
-calculate(50);
-calculate(20);
-calculate(10);
-
+buyProduct(50);
+buyProduct(20);
+buyProduct(10);
 
 // Задание №4: Создал функцию
 
-function flyAnotherCity(name) {
+function greetUser(name) {
   console.log(`Приветствую, ${name}! Добро пожаловать`);
 }
-flyAnotherCity('Мавлид');
+greetUser('Мавлид');
+
+// Задание №5: Создал 4 переменные.
 
 const firstFlight = 'Москва';
 const transFerAnotherPlane = 'Dubai';
