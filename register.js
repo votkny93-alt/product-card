2ab7d55e0e6267bd43fbf3b4f8d415d95d95c638
Mavlid <votkny93@gmail.com>
2026-07-27 21:24:43 +0300

    fix error

--- register.js
+++ register.js
@@ -10,53 +10,6 @@ const modalContent = document.getElementsByClassName('modal-content');
 const closeBtn = document.getElementById('closeModalBtn');
 const registerForm = document.getElementById('registerForm');
 
-openBtn.addEventListener('click', () => {
-  modal.classList.add('modal-showed')
-});
-
-function closeModal() {
-  modal.classList.remove('modal-showed');
-  registerForm.reset();
-}
-
-closeBtn.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-registerForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  if (!registerForm.checkValidity()) {
-    alert('Заполните все обязательные поля корректно!');
-    return;
-  }
-
-  const password = document.getElementById('regPassword').value;
-  const confirmPassword = document.getElementById('regConfirmPassword').value;
-  if (password !== confirmPassword) {
-    alert('Пароли не совпадают!');
-    return;
-  }
-
-  user = {
-    name: document.getElementById('regName').value,
-    lastName: document.getElementById('regLastName').value,
-    birthDate: document.getElementById('regDate').value,
-    login: document.getElementById('regLogin').value,
-    password: password,
-    createdOn: new Date()
-  };
-
-  closeModal();
-  alert('Регистрация прошла успешно!');
-
-  const isOpen = document.querySelector('.modal');
-
-  if (isOpen.classList.contains('is-open')) {
-
-  } else {
-  }
-});
-
 const regModal = new Modal('registrationModal');
 const myForm = new Form('registerForm');
 
@@ -68,7 +21,7 @@ registerForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
     if (myForm.isValid) {
-    const data = myForm.getValues();
+    const data = myForm.getValues;
     console.log(data);
 
     myForm.reset()
